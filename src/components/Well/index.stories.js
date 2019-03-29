ea96ac43e4f08a6f72779e4e8af2612d27ab3940
Chris Brett <chris.brett@citizensadvice.org.uk>
2019-03-29 10:21:41 +0000

    Refactor: well component to be used with any content.

--- src/components/Well/index.stories.js
+++ src/components/Well/index.stories.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
-import { storiesOf } from '@storybook/react';
-import Well from './index';
+import React, { Component } from "react";
+import { storiesOf } from "@storybook/react";
+import Well from "./index";
 
-
-storiesOf("Well", module)
-.add("default", () => (
+storiesOf("Well", module).add("default", () => (
   <div className="m-4">
-    <Well/>
+    <Well>
+      <h2 className="h2 mt-0">Hello World</h2>
+      <p className="lh-2 mb-0">
+        Get six alpha pups in here for a focus group today shall be a cloudy day, thanks to blue sky
+        thinking, we can now deploy our new ui to the cloud yet strategic fit.
+      </p>
+    </Well>
   </div>
-))
+));
